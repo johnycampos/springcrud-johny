49f895cd8f76300c72a634223a60a59e53fe284c
Johny Campos <johny.campos@ymail.com>
2021-07-18 20:17:38 -0300

    tudo funcionando

--- src/main/java/com/eventosapp/DataConfiguration.java
+++ src/main/java/com/eventosapp/DataConfiguration.java
@@ -31,5 +31,29 @@ public class DataConfiguration {
 
         return basicDataSource;
     }
+	
+	//Conectar com o banco localhost
+	
+//	@Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
+//	
+//	@Bean
+//	public JpaVendorAdapter jpaVendorAdapter(){
+//		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//		adapter.setDatabase(Database.MYSQL);
+//		adapter.setShowSql(true);
+//		adapter.setGenerateDdl(true);
+//		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+//		adapter.setPrepareConnection(true);
+//		return adapter;
+//	}
+	
 }
 
